58be64d7edd057dea63d47884d3f0d6b3d34bc14
AlexJackson01 <ajjackson22@gmail.com>
2023-05-04 13:19:32 +0100

    Added exercises

---
+++ loops.js
@@ -0,0 +1,39 @@
+
+// Exercise 1
+// Create a for loop that logs the numbers 4 to 156 ONLY to the console.
+
+
+
+// Exercise 2
+// Create an object that holds information on your favourite recipe. Write a loop that prints all values to the console.
+
+
+
+// Exercise 3 
+// You are provided with an empty array called numArray. Using a for... in loop, fill the array with all of the numbers from the numbers object if they are greater than or equal to 2.
+
+const numbers = {
+	num1: 1,
+	num2: 2,
+	num3: 5,
+	num4: 18
+  };
+  
+const numArray = [];
+
+
+
+// Exercise 4:
+// Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it should multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
+              
+
+  
+// Exercise 5: 
+// Loop through the following object. Log the data to the console as an array of arrays in the following format: [[key, value], [key,value]] etc. Hint: you can use Google if you get stuck ;)
+
+const studentPopulation = {
+  science: 25,
+  geography: 15,
+  history: 10,
+  music: 22
+}
